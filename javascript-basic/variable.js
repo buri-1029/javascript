dda939972476b22ea0f07adf73c664f3de4eb04f
김부희 <kimbuhui@naver.com>
2021-02-21 23:58:26 +0900

    JS : Function

--- javascript-basic/variable.js
+++ javascript-basic/variable.js
@@ -22,12 +22,12 @@ console.log(globalName);
 // var (don't ever use this!)
 // var hoisting (move declaration from bottom to top) 
 // 호이스팅 : 어디에 선언했는지 상관없이 항상 선언을 제일 위로 끌어 올리는 것
-// has no block scope
+// has no block scope : block scope를 철저히 무시 
 console.log(age);
 // undefined : 변수는 정의 되었지만 아직 값이 할당되지 않았을 때
 {
   age = 4;
-  var age;
+  var age; 
 }
 console.log(age);
 
@@ -39,7 +39,7 @@ const daysInWeek = 7;
 const maxNumber = 5;
 
 // Note!
-// Immutable data types: premitive types, frozen objects (i.e. object.freeze())
+// Immutable data types: primitive types, frozen objects (i.e. object.freeze())
 // Mutable data types: all objects by default are mutable in JS
 // favor immutable data type always for a few reasons: 
 // (웬만하면 값을 할당한 다음에 값이 변경되지 않는 데이터 타입을 사용해라.)
@@ -125,4 +125,4 @@ text = '7' + 5;
 console.log(`value: ${text}, type: ${typeof text}`); // 75 string
 text = '8' / '2';
 console.log(`value: ${text}, type: ${typeof text}`); // 4 number
-// console.log(text.charAt(0)); // error -> ts
+// console.log(text.charAt(0)); // dynamic type때문에 error -> ts
